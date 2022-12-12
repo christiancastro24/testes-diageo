aac0f0610356ceaa212a818884fecb0f1a547c46
Christian Silveira <christian_silveiracastro@outlook.com>
2022-12-12 17:28:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Teste</h1>
+
+      <div className="parent">
+        <div className="outer10">
+          <div className="inner10"></div>
+        </div>
+        <div class="months">
+        <div className="checked">&#10003;</div>
+        <div className="checked">&#10003;</div>
+        <div className="checked">&#10003;</div>
+        </div>
+        <div className="months-parent">
+          <p className="teste">Fevereiro</p>
+          <hr />
+          <p className="month">
+            Treinamento encerrado. Você não <br /> antingiu 80% do quiz
+          </p>
+
+          <p className="teste">Janeiro</p>
+          <hr />
+          <p className="month">
+            Treinamento encerrado. Você não <br /> antingiu 80% do quiz
+          </p>
+
+          <p className="teste">Dezembro</p>
+          <hr />
+          <p className="month">
+            Treinamento encerrado. Você não <br /> antingiu 80% do quiz
+          </p>
+        </div>
+      </div>
+      <br />
+      <br />
+
+      <div className="parent">
+        <div className="outer20">
+          <div className="inner20"></div>
+        </div>
+        <div className="months">
+        <div className="checked">&#10003;</div>
+        <div className="checked">&#10003;</div>
+        <div className="checked">&#10003;</div>
+        </div>
+        <div className="months-parent">
+          <p className="teste">Fevereiro</p>
+          <hr />
+          <p className="month">
+            Treinamento encerrado. Você não <br /> antingiu 80% do quiz
+          </p>
+
+          <p className="teste">Janeiro</p>
+          <hr />
+          <p className="month">
+            Treinamento encerrado. Você não <br /> antingiu 80% do quiz
+          </p>
+
+          <p className="teste">Dezembro</p>
+          <hr />
+          <p className="month">
+            Treinamento encerrado. Você não <br /> antingiu 80% do quiz
+          </p>
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <div className="parent">
+        <div className="outer30">
+          <div className="inner30"></div>
+        </div>
+        <div class="months">
+        <div className="checked">&#10003;</div>
+        <div className="checked">&#10003;</div>
+        <div className="checked">&#10003;</div>
+        </div>
+        <div className="months-parent">
+          <p className="teste">Fevereiro</p>
+          <hr />
+          <p className="month">
+            Treinamento encerrado. Você não <br /> antingiu 80% do quiz
+          </p>
+
+          <p className="teste">Janeiro</p>
+          <hr />
+          <p className="month">
+            Treinamento encerrado. Você não <br /> antingiu 80% do quiz
+          </p>
+
+          <p className="teste">Dezembro</p>
+          <hr />
+          <p className="month">
+            Treinamento encerrado. Você não <br /> antingiu 80% do quiz
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
